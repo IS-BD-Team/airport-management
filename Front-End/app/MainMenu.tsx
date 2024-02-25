3d676e68a3ea27980eccb8c37197236bb3192cd0
Barcena05 <3rnst020421@gmail.com>
2024-02-25 16:58:04 -0500

    feat:SideBar Added and Tabs modified

--- Front-End/app/MainMenu.tsx
+++ Front-End/app/MainMenu.tsx
@@ -3,12 +3,12 @@ import { useState } from "react"
 export default () => {
     const [toogle, setToogle] = useState(false);
     return (
-        <div className="w-full bg-[#f6f6f9] h-10 items-center flex flex-row">
+        <div className="w-full bg-[#f6f6f9] h-10 items-center lg:flex flex-row my-2 hidden">
             <ul className="flex flex-row topTabs justify-between w-full">
                 <li onMouseEnter={()=>setToogle(true)} onMouseLeave={()=>setToogle(false)}>
                     <a id="new-object" href="#" 
                     > + </a>
-                    {toogle && <ul className="absolute">
+                    {toogle && <ul className="absolute bg-[#f6f6f9] insideTab">
                         <li><a href="/">New issue</a></li>
                         <li><a href="/">New category</a></li>
                         <li><a href="/">New version</a></li>
@@ -18,46 +18,46 @@ export default () => {
                         <li><a href="/">New file</a></li>
                     </ul>}
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Overview</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Activity</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Issues</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Spent time</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Gantt</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Agile</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Backlog</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Calendar</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Documents</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Wiki</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Files</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Repository</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Settings</a>
                 </li>
-                <li>
+                <li className="hover:border-b-4 hover:border-b-[#614ba6]">
                     <a href="/">Issue templates</a>
                 </li>
             </ul>
