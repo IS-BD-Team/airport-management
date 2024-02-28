7eab792348f1ca3cb5aaf007b75897a910d9c8ae
emryscv <emryscv@gmail.com>
2024-02-28 12:43:01 -0500

    feat/SideBarItems

--- FrontEnd/app/components/SideBar.tsx
+++ FrontEnd/app/components/SideBar.tsx
@@ -1,13 +1,21 @@
+import aeropuertos from "../../public/aeropuerto (2).png";
+import instalaciones from "../../public/almacen.png";
+import servicios from "../../public/informacion.png";
+import reparaciones from "../../public/sistema.png";
+import aviones from "../../public/avion.png";
+
+import SideBarItem from "./SideBarItem";
+
 export default function SideBar(){
     return (
         <aside className="w-[15vw] absolute h-full border-r-2">
             <ul className="w-full p-2">
-                <li className="p-3">Aeropuertos</li>
-                <li className="p-3">Instalaciones</li>
-                <li className="p-3">Servicios</li>
-                <li className="p-3">Reparaciones</li>
-                <li className="p-3">Naves</li>
-                <li className="p-3">ETC....</li>
+                <SideBarItem text="Aeropuertos" icon={aeropuertos}/>
+                <SideBarItem text="Instalaciones" icon={instalaciones}/>
+                <SideBarItem text="Servicios" icon={servicios}/>
+                <SideBarItem text="Reparciones" icon={reparaciones}/>
+                <SideBarItem text="Naves" icon={aviones}/>
+                <SideBarItem text="ETC..." icon={aviones}/>
             </ul>
         </aside>
     );
