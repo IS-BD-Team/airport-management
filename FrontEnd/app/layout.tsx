0902f1685236eba1a00e7ae7df9767c14cb71d81
emryscv <emryscv@gmail.com>
2024-02-28 11:00:15 -0500

    fix/move navbar to components folder

--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TopMenu from "./topMenu";
-import Header from "./Header";
-import MainMenu from "./MainMenu";
+import TopMenu from "./components/topMenu";
+import Header from "./components/Header";
+import MainMenu from "./components/MainMenu";
+import Favicon from "./favicon.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="./favicon.png" />
+            </head>
             <body className={inter.className}>
                 <header>
                     <TopMenu />
