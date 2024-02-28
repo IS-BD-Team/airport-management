f891df2cd8cb0cd889a3e24b43be86976fd71c65
emryscv <emryscv@gmail.com>
2024-02-28 11:21:46 -0500

    feat/component

---
+++ FrontEnd/app/components/SideBar.tsx
@@ -0,0 +1,14 @@
+export default function SideBar(){
+    return (
+        <aside className="w-[15vw] absolute h-full border-r-2">
+            <ul className="w-full p-2">
+                <li className="p-3">Aeropuertos</li>
+                <li className="p-3">Instalaciones</li>
+                <li className="p-3">Servicios</li>
+                <li className="p-3">Reparaciones</li>
+                <li className="p-3">Naves</li>
+                <li className="p-3">ETC....</li>
+            </ul>
+        </aside>
+    );
+}
