5df7da1b62606bd36c8c7975266c8df4638f586e
emryscv <emryscv@gmail.com>
2024-03-10 13:51:30 -0400

    feat/CustomTable

--- FrontEnd/app/components/SideBar.tsx
+++ FrontEnd/app/components/SideBar.tsx
@@ -8,7 +8,7 @@ import SideBarItem from "./SideBarItem";
 
 export default function SideBar(){
     return (
-        <aside className="w-[15vw] h-full border-r-2">
+        <aside className="w-[15vw] h-full border-r-2 shadow-[inset_-9px_10px_20px_-15px_rgba(0,0,0,0.3)]">
             <ul className="w-full p-2">
                 <SideBarItem text="Aeropuertos" icon={aeropuertos}/>
                 <SideBarItem text="Instalaciones" icon={instalaciones}/>
