1a3b076047f5d7e5375c71d9d02afe1e40e7c929
Barcena05 <3rnst020421@gmail.com>
2024-03-10 20:09:21 -0400

    changed structure

--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TopMenu from "./topMenu";
-import Header from "./Header";
-import MainMenu from "./MainMenu";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +19,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <header>
-                    <TopMenu />
                     <Header />
-                    <MainMenu />
                 </header>
                 {children}
             </body>
