62c7319c27130cbb371c07b34d5850dfd13b6db6
Rafael Acosta <theGitNoob@gmail.com>
2024-03-12 14:19:58 -0400

    feat(Migrations): add user db table

--- backend/src/AirportManagement.Infrastructure/Migrations/20240312181839_UsserAdd.Designer.cs
+++ backend/src/AirportManagement.Infrastructure/Migrations/20240312181839_UsserAdd.Designer.cs
@@ -2,6 +2,7 @@
 using AirportManagement.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AirportManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(AirportManagementDbContext))]
-    partial class AirportManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240312181839_UsserAdd")]
+    partial class UsserAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
