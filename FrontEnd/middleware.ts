a6d76dd0dda0381a7a9d3197620e282bda007471
Barcena05 <3rnst020421@gmail.com>
2024-03-11 21:01:59 -0400

    feat-middleware

--- FrontEnd/middleware.ts
+++ FrontEnd/middleware.ts
@@ -1,11 +1,20 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import {jwtVerify} from  "jose";
 export async function middleware(request:NextRequest) {
-    const {status} = await NextResponse.next()
-    if (status === 200) {
-        return NextResponse.next()
+    const jwt = request.cookies.get("userToken");
+    console.log(jwt);
+    if (jwt===undefined) {
+        return NextResponse.redirect(new URL("/login", request.url));
     }
-    return NextResponse.redirect(new URL('/login', request.url))
+    try {
+        const {payload} = await jwtVerify(String(jwt), new TextEncoder().encode('secret'));
+        console.log(payload);
+        return NextResponse.next();
+    } catch (error) {
+        console.log(error);
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
 }
 export const config = {
     matcher:['/dashboard/:path*', '/']
