e1ec206db1436ea73bf0104e86d358d54127ddf8
Rafael Acosta <theGitNoob@gmail.com>
2024-03-12 12:02:33 -0400

    feat (CQRS): add airport create command/commandhandler

---
+++ backend/src/AirportManagement.Application/Airports/Commands/CreateAirport/CreateAirportCommandHandler.cs
@@ -0,0 +1,21 @@
+using AirportManagement.Application.Common.Interfaces;
+using AirportManagement.Application.Common.Interfaces.Persistence.Airports;
+using AirportManagement.Domain.Airports;
+using ErrorOr;
+using MediatR;
+
+namespace AirportManagement.Application.Airports.Commands.CreateAirport;
+
+public class CreateAirportCommandHandler(IAirportsRepository airportsRepository, IUnitOfWork unitOfWork)
+    : IRequestHandler<CreateAirportCommand, ErrorOr<Airport>>
+{
+    public async Task<ErrorOr<Airport>> Handle(CreateAirportCommand request, CancellationToken cancellationToken)
+    {
+        var airport = new Airport(request.Name, request.Address);
+
+        await airportsRepository.AddAirportAsync(airport);
+        await unitOfWork.CommitChangesAsync();
+
+        return airport;
+    }
+}
