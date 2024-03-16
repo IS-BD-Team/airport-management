88e4605028ae9104fed1febbb699b464f2c61cc4
Barcena05 <3rnst020421@gmail.com>
2024-03-16 12:31:35 -0400

    feat-login_update

--- FrontEnd/app/login/page.tsx
+++ FrontEnd/app/login/page.tsx
@@ -1,27 +1,50 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react'
+import { FormEvent } from 'react';
+import {serialize} from 'cookie';
 
 const Login: React.FC = () => {  
   const router = useRouter();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async(event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Add logic to process login here
     router.push('../dashboard')
     const formData = new FormData(event.currentTarget);
     const email = formData.get('email');
     const password = formData.get('password');
+        
+    // const request = await fetch('/api/login', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ email, password }),
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
+
+    // if (request.ok) {
+    //   const Coockies = request.headers.get('Set-Cookie');
+    //   console.log(Coockies);
+    //   const data = await request.json();
+    //   const token = data['token'];
+    //   const serialized = serialize('userToken',token,
+    //   {
+    //     httpOnly: true,
+    //     path: '/',
+    //   });
+    //   request.headers.append('Set-Cookie', serialized);
+    // } else {
+    //   console.error('Error al loguearse');
+    // }    
+    
     router.push('../dashboard/Home');
   };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col p-4 max-w-sm mx-auto mt-10 space-y-4 bg-white shadow-md rounded-lg">
       <div>
-        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+        <label htmlFor="eserEmail" className="block text-sm font-medium text-gray-700">Username</label>
         <input
-          id="username"
-          type="text"
+          id="userEmail"
+          type="email"
           className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
