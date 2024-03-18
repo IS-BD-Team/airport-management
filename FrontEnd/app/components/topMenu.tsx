461dc06498f5ccee08378106e8a62a1968338e94
Barcena05 <3rnst020421@gmail.com>
2024-03-17 20:02:45 -0400

    fix-top_menus_modified

--- FrontEnd/app/components/topMenu.tsx
+++ FrontEnd/app/components/topMenu.tsx
@@ -1,25 +1,13 @@
-'use client'
 export default function TopMenu() {
-    // Obtener todas las cookies como una cadena de texto
-    const cookies = document.cookie;
-
-    // Dividir la cadena en un array de cookies individuales
-    const cookiesArray = cookies.split('; ');
-
-    // Buscar una cookie específica por su nombre
-    const tokenCookie = cookiesArray.find(cookie => cookie.startsWith('UserEmail='));
-
-    // Extraer el valor de la cookie (si existe)
-    const email = tokenCookie ? tokenCookie.split('=')[1] : null;
-
+    
     return (
         <div className="lg:w-full lg:flex lg:justify-between lg:text-[12px] 
         bg-[#002951] text-white px-2 hidden">
 
-            <ul className="flex flex-row ">
-                <li className="mr-2 list-none">
+            <ul className="flex flex-row h-4">
+                {/* <li className="mr-2 list-none">
                     <a href="/dashboard/Home">Home</a>
-                </li>
+                </li> */}
                 {/* <li className="mr-2 list-none">
                     <a href="/dashboard/DataManagementSection">Data</a>
                 </li>
@@ -31,21 +19,21 @@ export default function TopMenu() {
                 </li>  */}
             </ul>
             <div className="flex flex-row ">
-                <div className="flex flex-row justify-between">
+                {/* <div className="flex flex-row justify-between">
                     <p className="mr-1.5">
                         Logged in as:
                     </p>
                     <p className="cursor-pointer">
                         {email}
                     </p>
-                </div>
+                </div> */}
                 <ul className="flex flex-row">
                     {/* <li className="ml-4 list-none">
                         <a href="/">My Account</a>
                     </li> */}
-                    <li className="ml-4 list-none">
+                    {/* <li className="ml-4 list-none">
                         <a href="/">Sign Out</a>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         </div>
