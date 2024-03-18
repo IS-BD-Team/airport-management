4ed0a3c1764a49a157c91250c293f9ae52f0db13
emryscv <emryscv@gmail.com>
2024-03-17 23:47:43 -0400

    feat/add more sidebar items

--- FrontEnd/app/components/SideBar.tsx
+++ FrontEnd/app/components/SideBar.tsx
@@ -3,6 +3,8 @@ import instalaciones from "../../public/almacen.png";
 import servicios from "../../public/informacion.png";
 import reparaciones from "../../public/sistema.png";
 import aviones from "../../public/avion.png";
+import clientes from "../../public/cliente.png"
+import administradores from "../../public/adminstrador.png"
 
 import SideBarItem from "./SideBarItem";
 
@@ -10,11 +12,12 @@ export default function SideBar(){
     return (
         <aside className="w-[15vw] h-full border-r-2 shadow-[inset_-9px_10px_20px_-15px_rgba(0,0,0,0.3)]">
             <ul className="w-full p-2">
+                <SideBarItem text="Adminstradores" icon={administradores}/>
                 <SideBarItem text="Aeropuertos" icon={aeropuertos}/>
                 <SideBarItem text="Instalaciones" icon={instalaciones}/>
                 <SideBarItem text="Servicios" icon={servicios}/>
                 <SideBarItem text="Reparciones" icon={reparaciones}/>
-                <SideBarItem text="Clientes" icon={aviones}/>
+                <SideBarItem text="Clientes" icon={clientes}/>
                 <SideBarItem text="Naves" icon={aviones}/>
             </ul>
         </aside>
