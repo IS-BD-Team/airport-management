0c81e95de597d42ed4818a8896312d523dfc682f
Rafael Acosta <theGitNoob@gmail.com>
2024-03-17 23:02:32 -0400

    fix(airports): add bearer header

--- FrontEnd/app/dashboard/DataManagementSection/page.tsx
+++ FrontEnd/app/dashboard/DataManagementSection/page.tsx
@@ -7,70 +7,70 @@ import { Instance } from "@/app/utils/types";
 import { useState } from "react";
 
 export default function DataManagement() {
+  const getAirportsData = async () => {
+    try {
+      const response = await fetch("http://localhost:5258/Airports/airports", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
 
-    const getAirportsData = async () => {
-        try {
-            const response = await fetch('http://localhost:5258/Airports/airports', {
-                method: 'GET',
-                headers: {
-                    'Authorization':''
-                }
-            })
-
-            return await response.json();
-        }
-        catch (err) {
-            console.log(err)
-        }
+      return await response.json();
+    } catch (err) {
+      console.log(err);
     }
-    //const data = getAirportsData();
-    const [toggleForm, setToogleForm] = useState(false);
-    const data = [{
-        id:"1",
-        nombre: "A",
-        ubicacion: "a",
-        posicionGeografica: "a",    
-    }];
-    const searchParams = useSearchParams();
-    const entity = searchParams.get("entity");
+  };
+  const data = getAirportsData();
+  const [toggleForm, setToogleForm] = useState(false);
 
-    if (entity == null) {
-        return (
-            <div className="m-5 relative">
-                <header className="flex justify-between">
-                    <h2 className="text-2xl font-bold">
-                        No se proporciono una entidad!!!
-                    </h2>
-                </header>
-            </div>
-        );
-        /*pedir a la bd */
-    } else {
-        return (
-            <div className="m-5 relative">
-                <header className="flex justify-between border-b-[1px] border-[#666] pb-1 border-solid">
-                    <h2 className="text-2xl font-bold">{entity}</h2>
-                    <button className="bg-gray-200 px-3 py-1 rounded-md" onClick={()=>setToogleForm(true)}>
-                        + Add
-                    </button>
-                </header>
+  const searchParams = useSearchParams();
+  const entity = searchParams.get("entity");
 
-                {toggleForm && <AddForm type={entity} handleToggleEvent={(e)=>{
-                    setToogleForm(false)
-                    e.preventDefault()
-                    }}/>}
+  if (entity == null) {
+    return (
+      <div className="m-5 relative">
+        <header className="flex justify-between">
+          <h2 className="text-2xl font-bold">
+            No se proporciono una entidad!!!
+          </h2>
+        </header>
+      </div>
+    );
+    /*pedir a la bd */
+  } else {
+    return (
+      <div className="m-5 relative">
+        <header className="flex justify-between border-b-[1px] border-[#666] pb-1 border-solid">
+          <h2 className="text-2xl font-bold">{entity}</h2>
+          <button
+            className="bg-gray-200 px-3 py-1 rounded-md"
+            onClick={() => setToogleForm(true)}
+          >
+            + Add
+          </button>
+        </header>
 
-                <fieldset>
-                    <legend>Filtros</legend>
-                </fieldset>
-                <section>
-                    <CustomTable
-                        data={[]}
-                        columnWidths={["20%", "10%", "10%", "50%", "10%"]}
-                    />
-                </section>
-            </div>
-        );
-    }
+        {toggleForm && (
+          <AddForm
+            type={entity}
+            handleToggleEvent={(e) => {
+              setToogleForm(false);
+              e.preventDefault();
+            }}
+          />
+        )}
+
+        <fieldset>
+          <legend>Filtros</legend>
+        </fieldset>
+        <section>
+          <CustomTable
+            data={[]}
+            columnWidths={["20%", "10%", "10%", "50%", "10%"]}
+          />
+        </section>
+      </div>
+    );
+  }
 }
-
