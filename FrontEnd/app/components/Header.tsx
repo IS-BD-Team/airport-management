fd59fa654d8d4e6d8ebddc806a6f5364cee55cf9
emryscv <emryscv@gmail.com>
2024-03-18 01:22:02 -0400

    refactor/user info bubble

--- FrontEnd/app/components/Header.tsx
+++ FrontEnd/app/components/Header.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -10,60 +10,80 @@ export default function Header() {
     const cookies = document.cookie;
 
     // Dividir la cadena en un array de cookies individuales
-    const cookiesArray = cookies.split('; ');
+    const cookiesArray = cookies.split("; ");
 
     // Buscar una cookie específica por su nombre
-    const emailCookie = cookiesArray.find(cookie => cookie.startsWith('UserEmail='));
-    const nameCookie = cookiesArray.find(cookie => cookie.startsWith('UserName='));
+    const emailCookie = cookiesArray.find((cookie) =>
+        cookie.startsWith("UserEmail=")
+    );
+    const nameCookie = cookiesArray.find((cookie) =>
+        cookie.startsWith("UserName=")
+    );
 
     // Extraer el valor de la cookie (si existe)
-    const email = emailCookie ? emailCookie.split('=')[1] : null;
-    const name = nameCookie ? nameCookie.split('=')[1] : null;
+    const email = emailCookie ? emailCookie.split("=")[1] : null;
+    const name = nameCookie ? nameCookie.split("=")[1] : null;
     const initial = name ? name[0] : "";
     const logout = () => {
-        document.cookie = "UserName" + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-        document.cookie = "UserEmail" + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-        document.cookie = "UserToken" + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-        router.push('/login');
-    }
+        document.cookie =
+            "UserName" + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+        document.cookie =
+            "UserEmail" + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+        document.cookie =
+            "UserToken" + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+        router.push("/login");
+    };
 
     return (
-        <div className="lg:w-full lg:flex lg:justify-between hidden pl-3 py-3 h-12 align-middle
-        text-white bg-[#005b7f]">
-            <div className="text-xl">
-                Aeropuertos
-            </div>
+        <div
+            className="lg:w-full lg:flex lg:justify-between hidden pl-3 py-3 h-12 align-middle
+        text-white bg-[#005b7f]"
+        >
+            <div className="text-xl">Aeropuertos</div>
             <div className="flex flex-row">
                 {/* <label htmlFor="search">Search:</label>
                 <input type="text" name="search" className="mr-2" /> */}
-                {!path.includes('login')&&<div className="relative animate-bounce w-6 h-6 mr-4 hover:animate-none">
-                    <div onClick={() => { SetDropdown(!showDropDown) }} className="cursor-pointer
-                bg-blue-500 text-black rounded-[9999px] w-6 h-6 text-center">
-                        {initial}
-                    </div>
-                    {showDropDown && <div className="absolute min-w-[300px] float-right right-0 top-[31px] dropdownShadow
-                bg-white text-black flex flex-col">
-                        {/* <div className="p-2 border-b border-b-slate-500">
+                {!path.includes("login") && (
+                    <div className="relative animate-bounce w-6 h-6 mr-4 hover:animate-none z-50">
+                        <div
+                            onClick={() => {
+                                SetDropdown(!showDropDown);
+                            }}
+                            className="cursor-pointer bg-blue-500 text-black rounded-[9999px] w-6 h-6 text-center"
+                        >
+                            {initial}
+                        </div>
+                        {showDropDown && (
+                            <div
+                                className="absolute min-w-[300px] float-right right-0 top-[31px]  p-2 rounded-lg dropdownShadow
+                bg-white text-black flex flex-col"
+                            >
+                                {/* <div className="p-2 border-b border-b-slate-500">
                             <input type="text" name="q" id="projects-quick-search" 
                             className="border border-slate-500 w-full"/>                        
                         </div> */}
-                        <div className=" border border-slate-500 flex flex-col">
-                            <p className="hover:bg-slate-300 border-b border-b-slate-500">
-                                Name: {name}
-                            </p>
-                            <p className="hover:bg-slate-300 border-b border-b-slate-500">
-                                Email: {email}
-                            </p>
-                            <button className="hover:bg-slate-300" onClick={()=>logout()}>
-                                Logout
-                            </button>
-                        </div>
-                        {/* <div className="p-2 border-b border-b-slate-500">
+                                <div className="flex flex-col p-2 rounded-lg">
+                                    <p className="hover:bg-slate-300"/*border-b border-b-slate-500"*/>
+                                        Name: {name}
+                                    </p>
+                                    <p className="hover:bg-slate-300" /*border-b border-b-slate-500"*/>
+                                        Email: {email}
+                                    </p>
+                                    <button
+                                        className="hover:bg-slate-300 bg-[#005b7f] text-white mt-2 rounded-lg" 
+                                        onClick={() => logout()}
+                                    >
+                                        Logout
+                                    </button>
+                                </div>
+                                {/* <div className="p-2 border-b border-b-slate-500">
                             <a href="/" className="dropdownLinks"><strong>All Projects</strong></a>
                         </div> */}
-                    </div>}
-                </div>}
+                            </div>
+                        )}
+                    </div>
+                )}
             </div>
         </div>
-    )
+    );
 }
--- FrontEnd/app/dashboard/DataManagementSection/page.tsx
+++ FrontEnd/app/dashboard/DataManagementSection/page.tsx
@@ -4,73 +4,94 @@ import { useSearchParams } from "next/navigation";
 import CustomTable from "./components/CustomTable";
 import AddForm from "./components/AddForm";
 import { Instance } from "@/app/utils/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { get } from "http";
 
 export default function DataManagement() {
-  const getAirportsData = async () => {
-    try {
-      const response = await fetch("http://localhost:5258/Airports/airports", {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      });
+    const [toggleForm, setToogleForm] = useState(false);
+    const [data, setData] = useState(null);
 
-      return await response.json();
-    } catch (err) {
-      console.log(err);
+    const getAirports = async () => {
+        try {
+            const response = await fetch(
+                "http://localhost:5258/Airports/airports",
+                {
+                    method: "GET",
+                    headers: {
+                        Authorization:
+                            "Bearer " + localStorage.getItem("token"),
+                    },
+                }
+            );
+
+            return response.json();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const getAirportsData = async () => {
+        const response = await getAirports();
+        console.log(response);
+        setData(response);
+    };
+
+    useEffect(() => {
+        getAirportsData();
+    }, []);
+
+    const searchParams = useSearchParams();
+    const entity = searchParams.get("entity");
+
+    if (entity == null) {
+        return (
+            <div className="m-5 relative">
+                <header className="flex justify-between">
+                    <h2 className="text-2xl font-bold">
+                        No se proporciono una entidad!!!
+                    </h2>
+                </header>
+            </div>
+        );
+        /*pedir a la bd */
+    } else {
+        return (
+            <div className="m-5 relative">
+                <header className="flex justify-between border-b-[1px] border-[#666] pb-1 border-solid">
+                    <h2 className="text-2xl font-bold">{entity}</h2>
+                    <button
+                        className="bg-gray-200 px-3 py-1 rounded-md"
+                        onClick={() => setToogleForm(true)}
+                    >
+                        + Add
+                    </button>
+                </header>
+
+                {toggleForm && (
+                    <AddForm
+                        type={entity}
+                        handleToggleEvent={(e) => {
+                            setToogleForm(false);
+                            e.preventDefault();
+                        }}
+                    />
+                )}
+
+                <fieldset>
+                    <legend>Filtros</legend>
+                </fieldset>
+                <section>
+                    {data != null && (
+                        <CustomTable
+                            data={data}
+                            columnWidths={["20%", "10%", "10%", "50%", "10%"]}
+                        />
+                    )}
+                    {data == null && (
+                        <h2 className="text-2xl font-bold text-center">No hay datos</h2>
+                    )}
+                </section>
+            </div>
+        );
     }
-  };
-  const data = getAirportsData();
-  const [toggleForm, setToogleForm] = useState(false);
-
-  const searchParams = useSearchParams();
-  const entity = searchParams.get("entity");
-
-  if (entity == null) {
-    return (
-      <div className="m-5 relative">
-        <header className="flex justify-between">
-          <h2 className="text-2xl font-bold">
-            No se proporciono una entidad!!!
-          </h2>
-        </header>
-      </div>
-    );
-    /*pedir a la bd */
-  } else {
-    return (
-      <div className="m-5 relative">
-        <header className="flex justify-between border-b-[1px] border-[#666] pb-1 border-solid">
-          <h2 className="text-2xl font-bold">{entity}</h2>
-          <button
-            className="bg-gray-200 px-3 py-1 rounded-md"
-            onClick={() => setToogleForm(true)}
-          >
-            + Add
-          </button>
-        </header>
-
-        {toggleForm && (
-          <AddForm
-            type={entity}
-            handleToggleEvent={(e) => {
-              setToogleForm(false);
-              e.preventDefault();
-            }}
-          />
-        )}
-
-        <fieldset>
-          <legend>Filtros</legend>
-        </fieldset>
-        <section>
-          <CustomTable
-            data={[]}
-            columnWidths={["20%", "10%", "10%", "50%", "10%"]}
-          />
-        </section>
-      </div>
-    );
-  }
 }
