5ee2620997645f184e7608443154aead87af1a35
Rafael Acosta <theGitNoob@gmail.com>
2024-03-19 09:27:04 -0400

    feat(Domain): add GeographicLocation field to Airport.cs

--- backend/src/AirportManagement.Domain/Airports/Airport.cs
+++ backend/src/AirportManagement.Domain/Airports/Airport.cs
@@ -2,11 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AirportManagement.Domain.Airports;
 
-public class Airport(string name, string address)
+public class Airport(string name, string address, string geographicLocation)
 {
     [Key] public int Id { get; set; }
 
     [Required] public string Name { get; set; } = name;
-
     [Required] public string Address { get; set; } = address;
+
+    [Required] public string GeographicLocation { get; set; } = geographicLocation;
 }
