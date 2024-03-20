47487aade9d5ee85450142d1a27b6d7a55dd3065
Barcena05 <3rnst020421@gmail.com>
2024-03-20 17:54:58 -0400

    fix:middleware

--- FrontEnd/middleware.ts
+++ FrontEnd/middleware.ts
@@ -2,14 +2,25 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request:NextRequest) {
-    const {status} = await NextResponse.next()
-    console.log(status);
-    if (status === 200) {
-        return NextResponse.next()
-    }
-    return NextResponse.redirect(new URL('/login', request.url))
+    // const response = await fetch('http://localhost:5258/auth/check',
+    // {
+    //     method: "GET",
+    //     headers: {
+    //         Authorization:
+    //             "Bearer " + localStorage.getItem("token"),
+    //     },
+    // })
+    // const status = await response.status;
+    // console.log('a');
+    // console.log(status);
+    // console.log('b');
+    // if (status === 200) {
+    //     return NextResponse.next()
+    // }
+    // return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.next();
 }
 
 export const config = {
-    matcher:['/dashboard/:path*', '/']
+    matcher:['/dashboard/:path*', '/', '/dashboard/Home'],
 }
