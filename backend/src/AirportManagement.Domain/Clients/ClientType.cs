7123220f06b70c1d5a72280c95fe2689bf539bde
Rafael Acosta <theGitNoob@gmail.com>
2024-03-21 11:36:00 -0400

    feat(Domain): add ClientType.cs enum

--- backend/src/AirportManagement.Domain/Clients/ClientType.cs
+++ backend/src/AirportManagement.Domain/Clients/ClientType.cs
@@ -2,10 +2,13 @@ using Ardalis.SmartEnum;
 
 namespace AirportManagement.Domain.Clients;
 
-public class ClientType(string name,int value):SmartEnum<ClientType>(name,value)
+public class ClientType(string name, int value) : SmartEnum<ClientType>(name, value)
 {
-    public static readonly ClientType Admin = new(nameof(Admin),0);
-    
-    public static readonly ClientType SuperAdmin = new(nameof(SuperAdmin),1);
-    
+    public static readonly ClientType Company = new(nameof(Company), 0);
+
+    public static readonly ClientType PlaneOwner = new(nameof(PlaneOwner), 1);
+
+    public static readonly ClientType Vip = new(nameof(Vip), 2);
+
+    public static readonly ClientType NormalClient = new(nameof(NormalClient), 3);
 }
