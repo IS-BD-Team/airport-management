b85155cbe51d0bae81d202b5f9aa8556d97ebd5f
Rafael Acosta <theGitNoob@gmail.com>
2024-03-21 14:24:01 -0400

    feat(Infrastructure): implement ClientRepository

---
+++ backend/src/AirportManagement.Infrastructure/Clients/Persistence/ClientRepository.cs
@@ -0,0 +1,39 @@
+using AirportManagement.Application.Common.Interfaces.Persistence.Clients;
+using AirportManagement.Domain.Clients;
+using AirportManagement.Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportManagement.Infrastructure.Clients.Persistence;
+
+public class ClientRepository(AirportManagementDbContext dbContext) : IClientRepository
+{
+    public async Task AddAsync(Client client)
+    {
+        await dbContext.Clients.AddAsync(client);
+    }
+
+    public async Task<Client?> DeleteAsync(int clientId)
+    {
+        var client = await dbContext.Clients.FindAsync(clientId);
+        if (client != null) dbContext.Clients.Remove(client);
+
+        return client;
+    }
+
+    public async Task<Client?> GetByIdAsync(int clientId)
+    {
+        return await dbContext.Clients.FindAsync(clientId);
+    }
+
+    public async Task<Client?> UpdateAsync(int clientId, Client newClientData)
+    {
+        var existingClient = await dbContext.Clients.FindAsync(clientId);
+        if (existingClient != null) existingClient = newClientData;
+        return existingClient;
+    }
+
+    public async Task<IEnumerable<Client>> GetAllAsync()
+    {
+        return await dbContext.Clients.ToListAsync();
+    }
+}
