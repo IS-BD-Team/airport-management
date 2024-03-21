8f63455152d567019b4405e7ea8294a7399994ca
Rafael Acosta <theGitNoob@gmail.com>
2024-03-21 11:35:45 -0400

    feat(Domain): add Client.cs correct structure

--- backend/src/AirportManagement.Domain/Clients/Client.cs
+++ backend/src/AirportManagement.Domain/Clients/Client.cs
@@ -1,22 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using AirportManagement.Domain.Planes;
 
 namespace AirportManagement.Domain.Clients;
 
-public class Client(string name, ClientType clientType, string ci, string country)
+public class Client(string name, string ci, string country, ArrivalRole arrivalRole, ClientType clientType)
 {
-    
-    [Key]
-    public int Id { get; set; }
-  
-    [Required]
-    public string Name { get; private set; } = name;
+    [Key] public int Id { get; set; }
 
-    [Required]
-    public string Ci { get; private set; } = ci;
+    [Required] public string Name { get; private set; } = name;
 
-    [Required]
-    public string Country { get; private set; } = country;
+    [Required] public string Ci { get; private set; } = ci;
 
-    [Required]
-    public ClientType ClientType { get; private set; } = clientType;
+    [Required] public string Country { get; private set; } = country;
+
+    [Required] public ClientType ClientType { get; private set; } = clientType;
+
+    [Required] public ArrivalRole ArrivalRole { get; set; } = arrivalRole;
+
+    public ICollection<Plane>? Planes { get; set; }
 }
