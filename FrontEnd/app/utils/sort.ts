c36cfd97a1f3760b99e4e0367c05ce21f82d6b46
Barcena05 <3rnst020421@gmail.com>
2024-03-22 13:05:06 -0400

    feat:sort

---
+++ FrontEnd/app/utils/sort.ts
@@ -0,0 +1,37 @@
+export default function Sort(data: [],
+    columnIndex: string) {
+    function sort(start: number, end: number):any[] {
+        let mid = Math.floor((start + end)/ 2);
+        if (start >= end) {
+            return data.slice(start, start+1);
+        }
+        const left = sort(start, mid);
+        const right = sort(mid+1 , end);
+        return merge(left, right);
+    }
+    function merge(left: any[], right: any[]):[] {
+        let temp:any[] = [];
+        let i = 0;
+        let j = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i][columnIndex] < right[j][columnIndex]) {
+                temp.push(left[i]);
+                i++;
+            } else {
+                temp.push(right[j]);
+                j++;
+            }
+        }
+        while (i < left.length) {
+            temp.push(left[i]);
+            i++;
+        }
+        while (j < right.length) {
+            temp.push(right[j]);
+            j++;
+        }
+        return temp as [];
+    }
+    return sort(0, data.length - 1);
+    
+}
