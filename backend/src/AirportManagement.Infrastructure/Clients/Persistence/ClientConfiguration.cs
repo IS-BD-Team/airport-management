72163ad80c416b899cacb9b003363b85a69122f2
Rafael Acosta <theGitNoob@gmail.com>
2024-03-21 23:02:15 -0400

    feat(Infrastructure): add db context config for client model

---
+++ backend/src/AirportManagement.Infrastructure/Clients/Persistence/ClientConfiguration.cs
@@ -0,0 +1,24 @@
+using AirportManagement.Domain.Clients;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirportManagement.Infrastructure.Clients.Persistence;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.HasKey(client => client.Id);
+        builder.HasIndex(client => client.Id);
+
+        builder.Property(client => client.ArrivalRole)
+            .HasConversion(
+                role => role.Value,
+                value => ArrivalRole.FromValue(value));
+
+        builder.Property(client => client.ClientType)
+            .HasConversion(
+                type => type.Value,
+                value => ClientType.FromValue(value));
+    }
+}
