990b7fbb0ee9421ad8fb7ffb864609df79014323
Barcena05 <3rnst020421@gmail.com>
2024-03-22 15:05:04 -0400

    fix:getData

--- FrontEnd/app/dashboard/DataManagementSection/components/CustomTable.tsx
+++ FrontEnd/app/dashboard/DataManagementSection/components/CustomTable.tsx
@@ -40,7 +40,7 @@ export default function CustomTable(prop: CustomTableProps) {
         <table className="w-full">
             <thead className="border-b-[2px] border-solid border-[#e3e5ec] w-full" style={{ backgroundColor: "#fff !important" }}>
                 <tr>
-                    {Object.keys(props.data[0]).map((item, index) => {
+                    {props.data.length>0&& Object.keys(props.data[0]).map((item, index) => {
                         return <th style={{ width: props.columnWidths[index] }}
                             className="text-left p-[1vw] " key={index}
                             onClick={() =>{
@@ -56,7 +56,7 @@ export default function CustomTable(prop: CustomTableProps) {
                 </tr>
             </thead>
             <tbody>
-                {props.data.map((row, i) => {
+                {props.data.length>0&& props.data.map((row, i) => {
                     return (
                         <tr key={i} className="h-[4vw]">
                             {Object.values(row).map((val, j) => {
