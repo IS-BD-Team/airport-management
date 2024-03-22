5cc80c2599989ee0b6242187fd661c4ede5b9bf8
Rafael Acosta <theGitNoob@gmail.com>
2024-03-21 23:00:51 -0400

    feat(Migrations): add new migration for client model

--- backend/src/AirportManagement.Infrastructure/Migrations/20240322024153_AddClientModel.Designer.cs
+++ backend/src/AirportManagement.Infrastructure/Migrations/20240322024153_AddClientModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AirportManagement.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AirportManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(AirportManagementDbContext))]
-    partial class AirportManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240322024153_AddClientModel")]
+    partial class AddClientModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
@@ -35,8 +38,6 @@ namespace AirportManagement.Infrastructure.Migrations
                         .HasColumnType("TEXT");
 
                     b.HasKey("Id");
-
-                    b.HasIndex("Id");
 
                     b.ToTable("Airports");
                 });
