bd9071a75573ca484a14b6194f9cb322326ee46c
Barcena05 <3rnst020421@gmail.com>
2024-03-21 20:14:42 -0400

    fix:Authorization

--- FrontEnd/middleware.ts
+++ FrontEnd/middleware.ts
@@ -2,23 +2,20 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request:NextRequest) {
-    // const response = await fetch('http://localhost:5258/auth/check',
-    // {
-    //     method: "GET",
-    //     headers: {
-    //         Authorization:
-    //             "Bearer " + localStorage.getItem("token"),
-    //     },
-    // })
-    // const status = await response.status;
-    // console.log('a');
-    // console.log(status);
-    // console.log('b');
-    // if (status === 200) {
-    //     return NextResponse.next()
-    // }
-    // return NextResponse.redirect(new URL('/login', request.url))
-    return NextResponse.next();
+    const response = await fetch('http://localhost:5258/auth/check',
+    {
+        method: "GET",
+        headers: {
+            Authorization:
+                "Bearer " + request.cookies.get('UserToken')?.value,
+        },
+    })
+    const status = await response.status;
+    if (status === 200) {
+        return NextResponse.next()
+    }
+    return NextResponse.redirect(new URL('/login', request.url));
+    // return NextResponse.next();
 }
 
 export const config = {
