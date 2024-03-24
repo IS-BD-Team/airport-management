d1a1c4634065093763d455fac9a2fd3524e82fef
Rafael Acosta <theGitNoob@gmail.com>
2024-03-24 11:44:48 -0400

    feat(Infrastructure): add airplane configuration

---
+++ backend/src/AirportManagement.Infrastructure/Airplanes/Persistence/AirPlaneConfiguration.cs
@@ -0,0 +1,37 @@
+using AirportManagement.Domain.Airplane;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirportManagement.Infrastructure.Airplanes.Persistence;
+
+public class AirPlaneConfiguration : IEntityTypeConfiguration<Airplane>
+{
+    public void Configure(EntityTypeBuilder<Airplane> builder)
+    {
+        builder.HasKey(plane => plane.Id);
+
+        builder.HasIndex(plane => plane.Id);
+
+        builder.Property(plane => plane.Classification).IsRequired();
+        builder.Property(plane => plane.ClientId).IsRequired();
+        builder.Property(plane => plane.MaxLoad).IsRequired();
+        builder.Property(plane => plane.ArriveDate)
+            .HasConversion(
+                offset => offset.ToString(),
+                s => DateTimeOffset.Parse(s))
+            .IsRequired();
+
+        builder.Property(plane => plane.DepartureDate)
+            .HasConversion(
+                offset => offset.ToString(),
+                s => DateTimeOffset.Parse(s))
+            .IsRequired();
+
+        builder.Property(plane => plane.DepartureDate).IsRequired();
+
+        builder.HasMany(plane => plane.Services)
+            .WithOne()
+            .HasForeignKey(service => service.FacilityId)
+            .IsRequired();
+    }
+}
