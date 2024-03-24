e1caf7d7597bb6f9d0538a754a448b61b46e416c
Rafael Acosta <theGitNoob@gmail.com>
2024-03-24 11:43:38 -0400

    fet(Application): create IAirplaneRepository

---
+++ backend/src/AirportManagement.Application/Common/Interfaces/Persistence/Airplanes/IAirplaneRepository.cs
@@ -0,0 +1,12 @@
+using AirportManagement.Domain.Airplane;
+
+namespace AirportManagement.Application.Common.Interfaces.Persistence.Airplanes;
+
+public interface IAirplaneRepository
+{
+    Task AddAAsync(Airplane airplane);
+    Task<Airplane?> DeleteAsync(int airplaneId);
+    Task<Airplane?> GetByIdAsync(int airplaneId);
+    Task<Airplane?> UpdateAsync(int airplaneId, Airplane airplane);
+    Task<IEnumerable<Airplane>> GetAllAsync();
+}
