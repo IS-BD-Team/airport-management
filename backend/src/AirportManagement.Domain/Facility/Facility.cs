9a87a82f07bfac7f81d672aa3748e4befaec068b
Rafael Acosta <theGitNoob@gmail.com>
2024-03-24 22:35:44 -0400

    feat(Domain): add service to the facility model

--- backend/src/AirportManagement.Domain/Facility/Facility.cs
+++ backend/src/AirportManagement.Domain/Facility/Facility.cs
@@ -1,9 +1,13 @@
+using AirportManagement.Domain.Services;
+
 namespace AirportManagement.Domain.Facility;
 
-public class Facility(int id, string? name, string type, string location)
+public class Facility(string? name, string type, string location)
 {
-    public int Id { get; set; } = id;
+    public int Id { get; set; }
     public string? Name { get; set; } = name;
     public string Type { get; set; } = type;
     public string Location { get; set; } = location;
+
+    private ICollection<Service>? Services { get; set; }
 }
