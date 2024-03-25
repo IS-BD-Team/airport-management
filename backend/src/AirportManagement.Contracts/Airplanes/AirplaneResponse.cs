1ea2fb8faf59aa11659d9cb5d0e6ab1aee92d92a
Rafael Acosta <theGitNoob@gmail.com>
2024-03-25 18:54:27 -0400

    feat(Contracts): update AirplaneResponse.cs ClientResponse.cs FacilityResponse.cs ServiceResponse.cs to show related entities

--- backend/src/AirportManagement.Contracts/Airplanes/AirplaneResponse.cs
+++ backend/src/AirportManagement.Contracts/Airplanes/AirplaneResponse.cs
@@ -1,11 +1,25 @@
+using AirportManagement.Contracts.Clients;
+
 namespace AirportManagement.Contracts.Airplanes;
 
-public record AirplaneResponse(
-    int Id,
-    string Classification,
-    int ClientId,
-    int MaxLoad,
-    int PassengersCapacity,
-    string ArriveDate,
-    string DepartureDate,
-    bool HasReceivedMaintenance);
+public class AirplaneResponse(
+    int id,
+    string classification,
+    int clientId,
+    int maxLoad,
+    int passengersCapacity,
+    string arriveDate,
+    string departureDate,
+    bool hasReceivedMaintenance,
+    ClientResponse? client = null)
+{
+    public int Id { get; } = id;
+    public string Classification { get; } = classification;
+    public int ClientId { get; } = clientId;
+    public int MaxLoad { get; } = maxLoad;
+    public int PassengersCapacity { get; } = passengersCapacity;
+    public string ArriveDate { get; } = arriveDate;
+    public string DepartureDate { get; } = departureDate;
+    public bool HasReceivedMaintenance { get; } = hasReceivedMaintenance;
+    public ClientResponse? Client { get; set; } = client;
+}
--- backend/src/AirportManagement.Contracts/Facilities/FacilityResponse.cs
+++ backend/src/AirportManagement.Contracts/Facilities/FacilityResponse.cs
@@ -1,7 +1,10 @@
+using AirportManagement.Contracts.Services;
+
 namespace AirportManagement.Contracts.Facilities;
 
 public record FacilityResponse(
     int FacilityId,
     string? Name,
     string Type,
-    string Location);
+    string Location,
+    List<ServiceResponse>? Services = null);
