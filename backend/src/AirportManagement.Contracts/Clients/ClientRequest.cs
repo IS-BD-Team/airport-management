ce05376a62aab79e9005734b2e5b2b5f1785b680
Rafael Acosta <theGitNoob@gmail.com>
2024-03-24 21:35:01 -0400

    refactor(Contracts): rename ClientRequest.cs

---
+++ backend/src/AirportManagement.Contracts/Clients/ClientRequest.cs
@@ -0,0 +1,8 @@
+namespace AirportManagement.Contracts.Clients;
+
+public record ClientRequest(
+    string Name,
+    string Ci,
+    string Country,
+    string ArrivalRole,
+    string ClientType);
