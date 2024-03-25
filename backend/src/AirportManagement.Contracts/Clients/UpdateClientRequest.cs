c6ebb3dba0c5ec4da50d5f93fe42f86e8892965e
Rafael Acosta <theGitNoob@gmail.com>
2024-03-24 21:29:19 -0400

    refactor(Contracts): remove unused request

--- backend/src/AirportManagement.Contracts/Clients/UpdateClientRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace AirportManagement.Contracts.Clients;
-
-public record UpdateClientRequest(
-    string Name,
-    string Ci,
-    string Country,
-    string ArrivalRole,
-    string ClientType);
