9ffbe5a8791822c04cd3cb22a273131b21e675f1
emryscv <emryscv@gmail.com>
2024-03-25 21:49:06 -0400

    feat/esta jugada de formatedData

--- FrontEnd/app/utils/types.ts
+++ FrontEnd/app/utils/types.ts
@@ -1,8 +1,11 @@
+import { StaticImageData } from "next/image";
+
 export type Aeropuerto = {
     id: string;
     nombre: string;
     direccion: string;
     posicionGeografica: string;
+    picture: StaticImageData;
 }
 export type Instalacion = {
     ida: string;
@@ -10,17 +13,20 @@ export type Instalacion = {
     nombre: string;
     tipo: string;
     ubicacion: string;
+    picture: StaticImageData;
 }
 export type Servicio = {
     id: string;
     descripcion: string;
     precio: number;
+    picture: StaticImageData;
 }
 export type Reparcion = {
     id: string;
     descripcion: string;
     precio: number;
     tipo: string;
+    picture: StaticImageData;
 }
 export type Cliente = {
     id: string;
@@ -28,6 +34,7 @@ export type Cliente = {
     nacionalidad: string; 
     tipo: string;
     hasNave: boolean;
+    picture: StaticImageData;
 }
 export type Nave = {
     matricula: string;
@@ -35,13 +42,30 @@ export type Nave = {
     clasificacion: string; 
     tripulantes: number;
     capacidad: string;
+    picture: StaticImageData;
 }
-
+export type Estancia = {
+    matricula: string;
+    fechaInicio: string;
+    idA: string;
+    valoracion: number;
+    fechaPropuestaFin: string;
+    picture: StaticImageData;
+}
+export type ReparacionNave = {
+    matricula: string;
+    fechaInicio: string;
+    codigo: string;
+    tiempo: number;
+    fechaFin: string;
+    picture: StaticImageData;
+}
 export type TestingType = {
     nombre: string, 
     grupo: string, 
     num: string, 
     description: string, 
     fecha: string,
+    picture: StaticImageData,
 }
-export type Instance = Aeropuerto | Instalacion | Servicio | Reparcion | Cliente | Nave; 
+export type Instance = Aeropuerto | Instalacion | Servicio | Reparcion | Cliente | Nave | Estancia | ReparacionNave | TestingType; 
