243aa2bb43f28ddfe83d64c7e93d45ea291e87c2
Rafael Acosta <theGitNoob@gmail.com>
2024-03-25 20:47:02 -0400

    feat(Infrastructure): add new migration for ClientRatings

--- backend/src/AirportManagement.Infrastructure/Migrations/20240325235944_CreateClientRatingEntity.Designer.cs
+++ backend/src/AirportManagement.Infrastructure/Migrations/20240325235944_CreateClientRatingEntity.Designer.cs
@@ -4,6 +4,7 @@ using AirportManagement.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AirportManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(AirportManagementDbContext))]
-    partial class AirportManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240325235944_CreateClientRatingEntity")]
+    partial class CreateClientRatingEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
