10746b1eace21c99308dcc8fc2cface36e27f68e
emryscv <emryscv@gmail.com>
2024-03-29 15:02:02 -0400

    feat/generic Create Instance

--- FrontEnd/app/utils/createInstance.ts
+++ FrontEnd/app/utils/createInstance.ts
@@ -5,80 +5,8 @@ export default async function createInstance(
     entity: string
 ) {
     event.preventDefault();
-    var name,
-        address,
-        geographicLocation,
-        airportId,
-        location,
-        type,
-        description,
-        price,
-        facilityId,
-        ci,
-        country,
-        clientType,
-        hasNave,
-        classification,
-        clientId,
-        maxLoad,
-        passengersCapacity,
-        crewMembers;
-    var body = {};
 
-    switch (entity) {
-        case "Administradores":
-            break;
-        case "Aeropuertos":
-            name = event.currentTarget["name"].value;
-            address = event.currentTarget["adress"].value;
-            geographicLocation = event.currentTarget["geographicLocation"].value;
-            body = { name, address, geographicLocation };
-            break;
-        case "Instalaciones":
-            airportId = event.currentTarget["adress"];
-            name = event.currentTarget["name"];
-            location = event.currentTarget["location"];
-            type = event.currentTarget["type"];
-            body = { name, type, location, airportId };
-        case "Servicios":
-            facilityId = event.currentTarget["facilityId"];
-            description = event.currentTarget["description"];
-            price = event.currentTarget["price"];
-            body = { facilityId, description, price };
-        case "Reparaciones":
-            facilityId = event.currentTarget["facilityId"];
-            description = event.currentTarget["description"];
-            price = event.currentTarget["price"];
-            type = event.currentTarget["type"];
-            body = { facilityId, description, price, type };
-        case "Clientes":
-            name = event.currentTarget["name"];
-            ci = event.currentTarget["ci"];
-            country = event.currentTarget["country"];
-            clientType = event.currentTarget["clientType"];
-            hasNave = event.currentTarget["hasNave"];
-            body = { name, ci, country, clientType };
-        case "Naves":
-            classification = event.currentTarget["classification"];
-            clientId = event.currentTarget["clasclientIdsification"];
-            maxLoad = event.currentTarget["maxLoad"];
-            passengersCapacity = event.currentTarget["passengersCapacity"];
-            crewMembers = event.currentTarget["crewMembers"];
-            body = {
-                classification,
-                clientId,
-                maxLoad,
-                passengersCapacity,
-                crewMembers,
-            };
-        case "Estancias":
-
-        case "ReparacionesNaves":
-
-        default:
-    }
-
-    console.log(`new ${entity}`, body);
+    const form = new FormData(event.currentTarget);
 
     try {
         const response = await fetch(getEndpoint(entity), {
@@ -87,7 +15,7 @@ export default async function createInstance(
                 "Content-Type": "application/json",
                 Authorization: "Bearer " + localStorage.getItem("token"),
             },
-            body: JSON.stringify(body),
+            body: JSON.stringify(Object.fromEntries(form.entries())),
         });
         console.log(response);
         window.location.reload();
