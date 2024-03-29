084875f80803f7b3a9cf6a80d1698a787557c558
Rafael Acosta <theGitNoob@gmail.com>
2024-03-28 22:37:07 -0400

    feat(Domain): Update AirplaneRepairService model

--- backend/src/AirportManagement.Domain/Services/AirplaneRepairService.cs
+++ backend/src/AirportManagement.Domain/Services/AirplaneRepairService.cs
@@ -1,12 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AirportManagement.Domain.Services;
 
-public class AirplaneRepairService(int id, string type, string description, decimal pricePerHour)
-    : RepairService(id, type, description, pricePerHour)
+public class AirplaneRepairService(int airPlaneId, int repairServiceId, DateTime startDate, DateTime endDate)
 {
-    [Required] public DateTimeOffset StartDate { get; set; }
-    [Required] public DateTimeOffset EndDate { get; set; }
+    [Key] public int Id { get; init; }
 
-    public decimal ElapsedHours => (decimal)(EndDate.TotalOffsetMinutes - StartDate.TotalOffsetMinutes) / 60;
+    [Required] public int AirPlaneId { get; set; } = airPlaneId;
+    [ForeignKey(nameof(AirPlaneId))] public Airplane.Airplane? Plane { get; set; }
+
+    [Required] public int RepairServiceId { get; set; } = repairServiceId;
+    [ForeignKey(nameof(RepairServiceId))] public RepairService RepairService { get; set; } = null!;
+
+    [Required] public DateTime StartDate { get; set; } = startDate;
+    [Required] public DateTime EndDate { get; set; } = endDate;
+
+    [Required] public DateTime CreationDate { get; private init; } = DateTime.UtcNow;
+
+
+    public decimal ElapsedHours => (decimal)(EndDate - StartDate).TotalHours;
 }
