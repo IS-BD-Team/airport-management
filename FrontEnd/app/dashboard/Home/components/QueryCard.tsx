73178917df24acaf2996aa256dbada5cd808c395
emryscv <emryscv@gmail.com>
2024-04-01 09:02:57 -0400

    feat/add link

--- FrontEnd/app/dashboard/Home/components/QueryCard.tsx
+++ FrontEnd/app/dashboard/Home/components/QueryCard.tsx
@@ -1,13 +1,19 @@
+import { link } from "fs";
+import Link from "next/link";
+
 type QueryCardProps = {
-    text: string;
-}
+  text: string;
+  to: string;
+};
 
-export default function QueryCard(props : QueryCardProps) {
-    return (
-        <div className="flex flex-col justify-between p-4 mx-auto space-y-4 bg-white shadow-md rounded-lg">
-            {props.text}
+export default function QueryCard(props: QueryCardProps) {
+  return (
+    <Link href={props.to}>
+      <div className="flex flex-col justify-between p-4 mx-auto space-y-4 bg-white shadow-md rounded-lg">
+        {props.text}
 
-            <button className="bg-sky-700 p-1 rounded-md">Consultar</button>
-        </div>
-    );
+        <button className="bg-sky-700 p-1 rounded-md">Consultar</button>
+      </div>
+    </Link>
+  );
 }
