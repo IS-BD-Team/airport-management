59fa61fe4853ca817cb18b3302482853c9ebb688
Rafael Acosta <theGitNoob@gmail.com>
2024-04-01 17:25:54 -0400

    refactor(Domain): rename plane property to airplane

--- backend/src/AirportManagement.Domain/AirplaneRepairService/AirplaneRepairService.cs
+++ backend/src/AirportManagement.Domain/AirplaneRepairService/AirplaneRepairService.cs
@@ -9,7 +9,7 @@ public class AirplaneRepairService(int airPlaneId, int repairServiceId, DateTime
     [Key] public int Id { get; init; }
 
     [Required] public int AirPlaneId { get; set; } = airPlaneId;
-    [ForeignKey(nameof(AirPlaneId))] public Airplane.Airplane? Plane { get; set; }
+    [ForeignKey(nameof(AirPlaneId))] public Airplane.Airplane? AirPlane { get; set; }
 
     [Required] public int RepairServiceId { get; set; } = repairServiceId;
     [ForeignKey(nameof(RepairServiceId))] public RepairService RepairService { get; set; } = null!;
