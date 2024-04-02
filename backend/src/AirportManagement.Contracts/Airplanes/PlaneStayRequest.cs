8ab31ce6f8f142d567086e7d754f5874fa391cc0
Rafael Acosta <theGitNoob@gmail.com>
2024-04-02 03:46:39 -0400

    feat(sln): update PlaneStay.cs rating

--- backend/src/AirportManagement.Contracts/Airplanes/PlaneStayRequest.cs
+++ backend/src/AirportManagement.Contracts/Airplanes/PlaneStayRequest.cs
@@ -1,3 +1,3 @@
 namespace AirportManagement.Contracts.Airplanes;
 
-public record PlaneStayRequest(int AirplaneId, int AirportId, DateTime ArrivalDate, DateTime DepartureDate);
+public record PlaneStayRequest(int AirplaneId, int AirportId, int Rating, DateTime ArrivalDate, DateTime DepartureDate);
